b3565f5fec6c2b858a7eabab0092304007df62c4
Official-Dev-Vineet <viratsinghkaharwar8923@gmail.com>
2022-11-28 21:28:43 +0530

    gitignore file added

---
+++ index.js
@@ -0,0 +1,22 @@
+let messages = document.querySelector('.messages');
+let inputMsg = document.querySelector('#msg');
+let status = document.querySelector('.status')
+
+inputMsg.addEventListener('keyup', (e) => {
+    if (e.key == 'Enter') {
+        e.target.value = ''
+        let msg = document.createElement('div');
+        msg.classList.add('msg-send')
+        msg.innerText = 'message sent.'
+        document.body.appendChild(msg);
+        setTimeout(()=>{
+            document.body.removeChild(msg);
+        },1500)
+    }
+    else {
+        status.innerText = 'typing...'
+        setTimeout(() => {
+            status.innerText = ''
+        }, 2000)
+    }
+})
